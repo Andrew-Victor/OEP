f2b9cf1c41ea298119bfabd04e66dced37e77e0e
Andrew victor <116605370+Andrew-Victor@users.noreply.github.com>
2024-06-27 19:28:58 +0300

    faceRecogntion

---
+++ CV microservice/face_Recogntion/main.py
@@ -0,0 +1,75 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+
+path = 'C:/Users/Dell/PycharmProjects/pythonProject4/venv/images'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+def get_images_from_folder(folder_path):
+    image_files = []
+    for root,dirs,files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root,file)
+            image_files.append(file_path)
+    return image_files
+
+new_image_path = 'C:/Users/victo/OneDrive/Desktop/Project/CV server RealTime Server/uploaded/12344/2222'
+images = get_images_from_folder(new_image_path)
+for image in images:
+    print(image)
+    img = cv2.imread(image)
+    if img is None:
+        print(f"Error: Unable to read the image at {new_image_path}")
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        matchIndex = np.argmin(faceDis)
+
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+        if faceDis[matchIndex] < 0.50:
+            name = classNames[matchIndex].upper()
+        else:
+            name = 'Unknown'
+        print(name)
+        y1, x2, y2, x1 = faceLoc
+        y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+    cv2.imshow('Input Image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
